634ca996ba1c1d60281b9774aa0ba18892e2ec2e
pinaki030 <pinaki.constacloud@gmail.com>
2020-05-23 13:12:55 +0530

    order api update

--- src/Ebaypackage.php
+++ src/Ebaypackage.php
@@ -5,52 +5,53 @@ namespace Ebaypackage;
 class Ebay
 {
 
-    public function getEbayOrders($token,$environment,$creationdate,$limit,$offset)
+   
+
+
+    public function getEbayOrders($token, $id = NULL, $PageNumber = 1, $OrdersPerPage = 100)
     {
-        if(!$token){
-            $error_response = '{
-                    "category": "REQUEST",
-                    "message": "Please enter vaild Token"
-            }';
-            return $error_response;
-        }else if(!$environment){
-            $error_response = '{
-                "category": "REQUEST",
-                "message": "Please enter vaild Environment"
-            }';
-            return $error_response;
-        }else{
-            if($environment != 'sandbox'){
-                $environment='';
-            }
-            if(!$creationdate){
-                $url = "https://api."."$environment".".ebay.com/sell/fulfillment/v1/order?";
-            }else{
-                $url = "https://api."."$environment".".ebay.com/sell/fulfillment/v1/order?filter=creationdate:"."$creationdate";
-            }
-            if($limit){
-                $url = "$url"."&limit="."$limit";
-                if($offset){
-                    $url =  "$url"."&offset="."$offset";
-                }
-            }
-           
-        }
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
         $headers = array();
-        $headers[] = "Authorization: Bearer .'$token'.";
-        $headers[] = "Accept: application/json";
-        $headers[] = "Content-Type: application/json";
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $result = curl_exec($ch);
-        if (curl_errno($ch)) {
-            echo 'Error:' . curl_error($ch);
+        $headers[] = "X-EBAY-API-SITEID:0";
+        $headers[] = "X-EBAY-API-COMPATIBILITY-LEVEL:967";
+        $headers[] = "X-EBAY-API-CALL-NAME:GetOrders";
+        $headers[] = "X-EBAY-API-IAF-TOKEN:".$token;
+        
+        if($id)
+        {
+            $postData = '<?xml version="1.0" encoding="utf-8"?>
+            <GetOrdersRequest xmlns="urn:ebay:apis:eBLBaseComponents">    
+            <ErrorLanguage>en_US</ErrorLanguage>
+            <WarningLevel>High</WarningLevel>
+            <OrderIDArray>
+            <OrderID>'.$id.'</OrderID>
+            </OrderIDArray>
+            </GetOrdersRequest>';
         }
-        curl_close ($ch);
-        return $result;
+        else
+        {
+            $postData = '<?xml version="1.0" encoding="utf-8"?>
+            <GetOrdersRequest xmlns="urn:ebay:apis:eBLBaseComponents">    
+            <ErrorLanguage>en_US</ErrorLanguage>
+            <WarningLevel>High</WarningLevel>
+            <CreateTimeFrom>'.date('Y-m-d\TH:i:s', strtotime('-1 months')).'</CreateTimeFrom>
+            <CreateTimeTo>'.date('Y-m-d\TH:i:s').'</CreateTimeTo>
+            <Pagination>
+            <EntriesPerPage>'.$OrdersPerPage.'</EntriesPerPage>
+            <PageNumber>'.$PageNumber.'</PageNumber>
+            </Pagination>
+            </GetOrdersRequest>';
+        }
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.ebay.com/ws/api.dll");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        
+        return json_encode(simplexml_load_string($response));
     }
 
 
